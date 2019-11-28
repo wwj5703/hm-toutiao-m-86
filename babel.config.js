c7cb8fa33ad2ded56103c585b7ceecf639b45366
wwj5703 <56508367+wwj5703@users.noreply.github.com>
2019-11-28 16:48:49 +0800

    项目初始化

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,8 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
+  // devServer: {// 实时保存、编译的配置段
+  //   open: true, // 自动开启浏览器
+  //   port: 12309 // 服务运行端口
+  // }
 }
---
+++ src/views/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <!--  顶部导航  -->
+
+      <!--  底部tab栏  -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style lang="less" scoped>
+</style>
